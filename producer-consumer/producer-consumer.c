fb305cb101f5cf312478ea385a338630640b1d42
HenriqueProj <henrique.caroco3@gmail.com>
2023-01-14 12:51:36 +0000

    dassd

--- producer-consumer/producer-consumer.c
+++ producer-consumer/producer-consumer.c
@@ -26,7 +26,38 @@ int pcq_create(pc_queue_t *queue, size_t capacity){
     queue->pcq_head = capacity;
     queue->pcq_tail = capacity;
 
-    return 1;
+    // Mutexes init
+    if(pthread_mutex_init(&queue->pcq_current_size_lock, NULL) != 0){
+        printf("[ERR]: Mutex init\n");
+        return -1;
+    }
+    if(pthread_mutex_init(&queue->pcq_head_lock, NULL) != 0){
+        printf("[ERR]: Mutex init\n");
+        return -1;
+    }
+    if(pthread_mutex_init(&queue->pcq_tail_lock, NULL) != 0){
+        printf("[ERR]: Mutex init\n");
+        return -1;
+    }
+    if(pthread_mutex_init(&queue->pcq_pusher_condvar_lock, NULL) != 0){
+        printf("[ERR]: Mutex init\n");
+        return -1;
+    }
+    if(pthread_mutex_init(&queue->pcq_popper_condvar_lock, NULL) != 0){
+        printf("[ERR]: Mutex init\n");
+        return -1;
+    }
+    //Condvars init
+    if(pthread_cond_init(&queue->pcq_pusher_condvar, NULL) != 0){
+        printf("[ERR]: Condvar init\n");
+        return -1;
+    }
+    if(pthread_cond_init(&queue->pcq_popper_condvar, NULL) != 0){
+        printf("[ERR]: Condvar init\n");
+        return -1;
+    }
+
+    return 0;
 }
 
 // pcq_destroy: releases the internal resources of the queue
@@ -41,7 +72,38 @@ int pcq_destroy(pc_queue_t *queue){
 
     queue->pcq_head = 0;
     queue->pcq_tail = 0;
-    return 1;
+    
+    if(pthread_mutex_destroy(&queue->pcq_current_size_lock) != 0){
+        printf("[ERR]: Mutex destroy\n");
+        return -1;
+    }
+    if(pthread_mutex_destroy(&queue->pcq_head_lock) != 0){
+        printf("[ERR]: Mutex destroy\n");
+        return -1;
+    }
+    if(pthread_mutex_destroy(&queue->pcq_tail_lock) != 0){
+        printf("[ERR]: Mutex destroy\n");
+        return -1;
+    }
+    if(pthread_mutex_destroy(&queue->pcq_pusher_condvar_lock) != 0){
+        printf("[ERR]: Mutex destroy\n");
+        return -1;
+    }
+    if(pthread_mutex_destroy(&queue->pcq_popper_condvar_lock) != 0){
+        printf("[ERR]: Mutex destroy\n");
+        return -1;
+    }
+    //Condvars destroy
+    if(pthread_cond_destroy(&queue->pcq_pusher_condvar) != 0){
+        printf("[ERR]: Condvar destroy\n");
+        return -1;
+    }
+    if(pthread_cond_destroy(&queue->pcq_popper_condvar) != 0){
+        printf("[ERR]: Condvar destroy\n");
+        return -1;
+    }
+    
+    return 0;
 }
 
 // pcq_enqueue: insert a new element at the front of the queue
